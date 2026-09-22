9c5537575e7f144af3dffdaeeb36c5c6f3e93dae
automacaopostcmb <automacaopostcmb@gmail.com>
2026-09-22 13:44:58 -0300

    Update learning.js

--- learning.js
+++ learning.js
@@ -84,7 +84,7 @@ const learningData = {
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         descricao:
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vitae lectus quis mauris tincidunt gravida."
-      }
+      },
 
       {
         professor: "Nome do professor 4.5",
